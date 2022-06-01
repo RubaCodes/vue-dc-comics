b7040069e1b4a6b92b549dcd347d448eb284af4a
rubacodes <rubacodes@gmail.com>
2022-06-01 15:18:36 +0200

    creazione main

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>to be filled....</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'BaseMain',
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  background-color: #222222;
+  color: white;
+  padding: 20px 0;
+  .container {
+    width: 1200px;
+    margin: auto;
+  }
+}
+</style>
