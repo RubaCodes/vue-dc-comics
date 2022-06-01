dec1c7caed2efd7bccdf9b3eae7d25dbb5f31997
rubacodes <rubacodes@gmail.com>
2022-06-01 15:03:03 +0200

    layout html header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>test</h1>
+  <header>
+    <div class="header__left">
+      <img src="../assets/img/dc-logo.png" alt="logo dc small" />
+    </div>
+    <div class="header__right">
+      <nav>
+        <ul>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
