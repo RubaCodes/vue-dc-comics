2f2925a9f0993a119b1f61f772fbf6dc38c83f27
rubacodes <rubacodes@gmail.com>
2022-06-01 15:43:27 +0200

    creazione html footer-top

--- src/components/footer/BaseFooterTop.vue
+++ src/components/footer/BaseFooterTop.vue
@@ -1,6 +1,49 @@
 <template>
   <div class="footer__top">
-    <h1>footer top</h1>
+    <div class="container">
+      <ul>
+        <li>
+          <div class="card">
+            <img
+              src="../../assets/img/buy-comics-digital-comics.png"
+              alt="buy-digital-comics"
+            /><a href="#">link</a>
+          </div>
+        </li>
+        <li>
+          <div class="card">
+            <img
+              src="../../assets/img/buy-comics-merchandise.png"
+              alt="buy-comics-merchandise"
+            /><a href="#">link</a>
+          </div>
+        </li>
+        <li>
+          <div class="card">
+            <img
+              src="../../assets/img/buy-comics-subscriptions.png"
+              alt="buy-comics-subscriptions"
+            /><a href="#">link</a>
+          </div>
+        </li>
+        <li>
+          <div class="card">
+            <img
+              src="../../assets/img/buy-comics-shop-locator.png"
+              alt="buy-comics-shop-locator"
+            /><a href="#">link</a>
+          </div>
+        </li>
+        <li>
+          <div class="card">
+            <img
+              src="../../assets/img/buy-dc-power-visa.svg"
+              alt="buy-dc-power-visa"
+            /><a href="#">link</a>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -10,4 +53,13 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer__top {
+  background-color: blue;
+  color: white;
+  .container {
+    width: 1200px;
+    margin: auto;
+  }
+}
+</style>
